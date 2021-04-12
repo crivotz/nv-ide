dc97f5f8e27f6b5282b2ba4a27af99d2ad048813
Mauro Locatelli <3275600+crivotz@users.noreply.github.com>
2021-04-12 12:17:10 +0200

    Added config for lsp-kind. New screenshots

---
+++ lua/plugins/lspkind.lua
@@ -0,0 +1,26 @@
+-- commented options are defaults
+require('lspkind').init({
+    -- with_text = true,
+    -- symbol_map = {
+    --   Text = '',
+    --   Method = 'ƒ',
+    --   Function = '',
+    --   Constructor = '',
+    --   Variable = '',
+    --   Class = '',
+    --   Interface = 'ﰮ',
+    --   Module = '',
+    --   Property = '',
+    --   Unit = '',
+    --   Value = '',
+    --   Enum = '了',
+    --   Keyword = '',
+    --   Snippet = '﬌',
+    --   Color = '',
+    --   File = '',
+    --   Folder = '',
+    --   EnumMember = '',
+    --   Constant = '',
+    --   Struct = ''
+    -- },
+})
