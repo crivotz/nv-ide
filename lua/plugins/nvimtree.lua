15ccc43fcd2db5da83cd92624847da9e00e49cd1
Mauro Locatelli <3275600+crivotz@users.noreply.github.com>
2022-03-14 14:55:36 +0100

    auto update folder nvimtree on opened file

--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -46,10 +46,10 @@ require'nvim-tree'.setup {
   -- update the focused file on `BufEnter`, un-collapses the folders recursively until it finds the file
   update_focused_file = {
     -- enables the feature
-    enable      = false,
+    enable      = true,
     -- update the root directory of the tree to the one of the folder containing the file if the file is not under the current root directory
     -- only relevant when `update_focused_file.enable` is true
-    update_cwd  = false,
+    update_cwd  = true,
     -- list of buffer names / filetypes that will not update the cwd if the file isn't found under the current root directory
     -- only relevant when `update_focused_file.update_cwd` is true and `update_focused_file.enable` is true
     ignore_list = {}
