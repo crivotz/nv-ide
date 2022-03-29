afb7a82232417816e417dd201b8b1ead86fda89f
Mauro Locatelli <3275600+crivotz@users.noreply.github.com>
2022-03-29 17:49:29 +0200

    More info for gruvbox-material

--- lua/settings/color.lua
+++ lua/settings/color.lua
@@ -3,6 +3,8 @@ if vim.fn.has("termguicolors") == 1 then
   vim.go.t_8b = "[[48;2;%lu;%lu;%lum"
   vim.opt.termguicolors = true
 end
+vim.g.gruvbox_material_background = "medium" -- hard, soft, medium
+vim.g.gruvbox_material_palette = "mix" -- original, mix, material
 vim.g.gruvbox_material_enable_italic = 1
 vim.g.gruvbox_material_sign_column_background = 'none'
 vim.cmd 'color gruvbox-material'
