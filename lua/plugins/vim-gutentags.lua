c15e548dd20e4708e09c30d9d06a791cdaca1b5f
Mauro Locatelli <3275600+crivotz@users.noreply.github.com>
2022-05-30 11:01:03 +0200

    Fixed problem with extra params from ripper-tags. Closed #15

--- lua/plugins/vim-gutentags.lua
+++ lua/plugins/vim-gutentags.lua
@@ -8,4 +8,5 @@ vim.g.gutentags_project_root = { '.git' }
 vim.g.gutentags_ctags_extra_args = { '--fields=+l' }
 vim.g.gutentags_add_default_project_roots = 0
 vim.g.gutentags_ctags_executable_ruby = 'ripper-tags'
-vim.g.gutentags_ctags_extra_args = { '--ignore-unsupported-options', '--recursive' }
+vim.g.gutentags_ctags_extra_args_ruby = { '--ignore-unsupported-options', '--recursive' }
+vim.g.gutentags_trace = 1
