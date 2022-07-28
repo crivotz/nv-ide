29abd4c4200272ed38f8ae55825f68c06768d72e
Mauro Locatelli <3275600+crivotz@users.noreply.github.com>
2022-07-28 09:12:11 +0200

    Updated with convenient executable for node dap

--- lua/plugins/nvim-dap.lua
+++ lua/plugins/nvim-dap.lua
@@ -4,9 +4,10 @@ vim.fn.sign_define('DapBreakpoint', {text='', texthl='error', linehl='', numh
 -- ADAPTERS
 dap.adapters.node2 = {
   type = 'executable',
-  command = 'node',
+  command = 'node-debug2-adapter',
   -- args = {os.getenv('HOME') .. '/.zinit/plugins/microsoft---vscode-node-debug2.git/out/src/nodeDebug.js'},
-  args =  { vim.fn.stdpath('data') .. '/mason/packages/node-debug2-adapter/out/src/nodeDebug.js' },
+  -- args =  { vim.fn.stdpath('data') .. '/mason/packages/node-debug2-adapter/out/src/nodeDebug.js' },
+  args = {},
 }
 dap.configurations.javascript = {
   {
