4b55959112ace40bf289c9a0d3625f58168969d5
Mauro Locatelli <3275600+crivotz@users.noreply.github.com>
2022-09-14 16:28:16 +0200

    Deprecation, temporary disabled markid

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -41,7 +41,6 @@ require("gitsigns").setup {
   current_line_blame_opts = {
     virt_text = true,
     virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
-    delay = 1000,
     ignore_whitespace = false,
   },
   current_line_blame_formatter_opts = {
