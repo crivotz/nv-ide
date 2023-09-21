5f64e2c6f58705ed8bfb56230c356b6989e1d39f
Mauro Locatelli <3275600+crivotz@users.noreply.github.com>
2023-09-21 15:35:15 +0200

    Re-added indent for test

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -135,6 +135,44 @@ return {
       })
     end
   },
+  --[[ { ]]
+  --[[   'lukas-reineke/indent-blankline.nvim', ]]
+  --[[   branch = 'v3', ]]
+  --[[   event = 'UIEnter', ]]
+  --[[   opts = { ]]
+  --[[     exclude = { ]]
+  --[[       -- stylua: ignore ]]
+  --[[       filetypes = { ]]
+  --[[         'dbout', 'neo-tree-popup', 'log', 'gitcommit', ]]
+  --[[         'txt', 'help', 'NvimTree', 'git', 'flutterToolsOutline', ]]
+  --[[         'undotree', 'markdown', 'norg', 'org', 'orgagenda', ]]
+  --[[       }, ]]
+  --[[     }, ]]
+  --[[     indent = { ]]
+  --[[       char = '│', -- ▏┆ ┊  ]]
+  --[[       tab_char = '│', ]]
+  --[[     }, ]]
+  --[[     scope = { ]]
+  --[[       char = '▎', ]]
+  --[[       highlight = { ]]
+  --[[         'RainbowDelimiterRed', ]]
+  --[[         'RainbowDelimiterYellow', ]]
+  --[[         'RainbowDelimiterBlue', ]]
+  --[[         'RainbowDelimiterOrange', ]]
+  --[[         'RainbowDelimiterGreen', ]]
+  --[[         'RainbowDelimiterViolet', ]]
+  --[[         'RainbowDelimiterCyan', ]]
+  --[[       }, ]]
+  --[[     }, ]]
+  --[[     show_first_indent_level = true, ]]
+  --[[   }, ]]
+  --[[   config = function(_, opts) ]]
+  --[[     require('ibl').setup(opts) ]]
+  --[[     local hooks = require('ibl.hooks') ]]
+  --[[     hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark) ]]
+  --[[     hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_space_indent_level) ]]
+  --[[   end, ]]
+  --[[ }, ]]
   {
     "shellRaining/hlchunk.nvim",
     event = { "UIEnter" },
@@ -588,5 +626,5 @@ return {
     '2kabhishek/co-author.nvim',
     dependencies = { 'stevearc/dressing.nvim' },
     cmd = 'GitCoAuthors',
-},
+}
 }
