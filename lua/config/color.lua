62d1c59d385e60c4f746e94f1fa2272ad6576154
Mauro Locatelli <3275600+crivotz@users.noreply.github.com>
2023-10-26 15:26:40 +0200

    Fix color

--- lua/config/color.lua
+++ lua/config/color.lua
@@ -1,5 +1,5 @@
 if vim.fn.has("termguicolors") == 1 then
-  vim.go.t_8f = "[[38;2;%lu;%lu;%lum"
-  vim.go.t_8b = "[[48;2;%lu;%lu;%lum"
+  --[[ vim.t_8f = "[[38;2;%lu;%lu;%lum" ]]
+  --[[ vim.t_8b = "[[48;2;%lu;%lu;%lum" ]]
   vim.opt.termguicolors = true
 end
