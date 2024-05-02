52130e86baf6e0cdb31baf53d0ae6726544ee62c
Mauro Locatelli <3275600+crivotz@users.noreply.github.com>
2024-05-02 15:23:49 +0200

    Test ok colors, better cyberdream

--- lua/config/colorschemes/cyberdream.lua
+++ lua/config/colorschemes/cyberdream.lua
@@ -2,7 +2,7 @@ require("cyberdream").setup({
   transparent = true,
   italic_comments = true,
   -- hide_fillchars = true,
-  borderless_telescope = true,
+  borderless_telescope = false,
   terminal_colors = true,
 })
 vim.cmd("colorscheme cyberdream")
