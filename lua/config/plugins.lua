a4bd3513d292074817b0c2b1f11977e09f382bb0
Mauro Locatelli <3275600+crivotz@users.noreply.github.com>
2024-05-13 12:49:33 +0200

    Added Octo.nvim

--- lua/config/plugins.lua
+++ lua/config/plugins.lua
@@ -524,7 +524,21 @@ return {
       "nvim-lua/plenary.nvim",
       "sindrets/diffview.nvim",
       "nvim-telescope/telescope.nvim",
+      -- "ibhagwan/fzf-lua",
     },
     config = true
+  },
+  {
+    "pwntester/octo.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+      -- "ibhagwan/fzf-lua",
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function ()
+      require"octo".setup()
+    end
   }
 }
