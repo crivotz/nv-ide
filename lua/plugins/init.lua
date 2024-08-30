c606849fab77a11c3ef60100555b9c095a5956c1
Mauro Locatelli <3275600+crivotz@users.noreply.github.com>
2024-08-30 17:06:23 +0200

    Test avante with claude

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -543,4 +543,49 @@ return {
       }
     end,
   },
+  {
+"yetone/avante.nvim",
+event = "VeryLazy",
+lazy = false,
+opts = {
+  -- add any opts here
+},
+keys = {
+  { "<leader>aa", function() require("avante.api").ask() end, desc = "avante: ask", mode = { "n", "v" } },
+  { "<leader>ar", function() require("avante.api").refresh() end, desc = "avante: refresh" },
+  { "<leader>ae", function() require("avante.api").edit() end, desc = "avante: edit", mode = "v" },
+},
+dependencies = {
+  "stevearc/dressing.nvim",
+  "nvim-lua/plenary.nvim",
+  "MunifTanjim/nui.nvim",
+  --- The below dependencies are optional,
+  "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+  {
+    -- support for image pasting
+    "HakonHarnes/img-clip.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- recommended settings
+      default = {
+        embed_image_as_base64 = false,
+        prompt_for_file_name = false,
+        drag_and_drop = {
+          insert_mode = true,
+        },
+        -- required for Windows users
+        use_absolute_path = true,
+      },
+    },
+  },
+  {
+    -- Make sure to setup it properly if you have lazy=true
+    'MeanderingProgrammer/render-markdown.nvim',
+    opts = {
+      file_types = { "markdown", "Avante" },
+    },
+    ft = { "markdown", "Avante" },
+  },
+},
+}
 }
