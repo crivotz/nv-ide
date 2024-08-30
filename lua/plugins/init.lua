636c4452961c22546d3465dc669d87353640871f
Mauro Locatelli <3275600+crivotz@users.noreply.github.com>
2024-08-30 17:10:58 +0200

    Test Avante w Claude

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -539,24 +539,24 @@ return {
         opts = {
           -- recommended settings
           default = {
-          embed_image_as_base64 = false,
-          prompt_for_file_name = false,
-          drag_and_drop = {
-            insert_mode = true,
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true,
           },
-          -- required for Windows users
-          use_absolute_path = true,
         },
       },
-    },
-    {
-      -- Make sure to setup it properly if you have lazy=true
-      'MeanderingProgrammer/render-markdown.nvim',
-      opts = {
-        file_types = { "markdown", "Avante" },
+      {
+        -- Make sure to setup it properly if you have lazy=true
+        'MeanderingProgrammer/render-markdown.nvim',
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
       },
-      ft = { "markdown", "Avante" },
     },
-  },
   }
 }
