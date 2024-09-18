c26fa5d896d39ad2a276643dfefdd24a1876f0d2
Mauro Locatelli <3275600+crivotz@users.noreply.github.com>
2024-09-18 14:27:07 +0200

    Disabled avante

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -468,6 +468,7 @@ return {
   {
     "yetone/avante.nvim",
     event = "VeryLazy",
+    enabled = false,
     lazy = false,
     version = false, -- set this if you want to always pull the latest change
     opts = {
