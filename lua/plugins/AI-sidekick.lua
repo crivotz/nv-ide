92b442bdebaa0a5fda9eac6049aad3b7b4891315
Mauro Locatelli <3275600+crivotz@users.noreply.github.com>
2025-10-06 16:33:46 +0200

    AI sidekick

---
+++ lua/plugins/AI-sidekick.lua
@@ -0,0 +1,49 @@
+return {
+  "folke/sidekick.nvim",
+  opts = {
+    -- add any options here
+    cli = {
+      mux = {
+        backend = "tmux",
+        enabled = true,
+      },
+    },
+  },
+  keys = {
+    {
+      "<tab>",
+      function()
+        -- if there is a next edit, jump to it, otherwise apply it if any
+        if not require("sidekick").nes_jump_or_apply() then
+          return "<Tab>" -- fallback to normal tab
+        end
+      end,
+      expr = true,
+      desc = "Goto/Apply Next Edit Suggestion",
+    },
+    {
+      "<c-.>",
+      function()
+        require("sidekick.cli").focus()
+      end,
+      desc = "Sidekick Switch Focus",
+      mode = { "n", "v" },
+    },
+    {
+      "<leader>aa",
+      function()
+        require("sidekick.cli").toggle({ name = "copilot", focus = true })
+      end,
+      desc = "Sidekick Toggle CLI",
+      mode = { "n", "v" },
+    },
+    {
+      "<leader>ap",
+      function()
+        require("sidekick.cli").select_prompt()
+      end,
+      desc = "Sidekick Ask Prompt",
+      mode = { "n", "v" },
+    },
+  },
+}
