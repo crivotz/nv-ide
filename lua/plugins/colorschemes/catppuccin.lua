09c36f047ec33c4b4a621de90b8769a47bf4df8e
Mauro Locatelli <3275600+crivotz@users.noreply.github.com>
2025-10-23 15:45:09 +0200

    Catppuccin

---
+++ lua/plugins/colorschemes/catppuccin.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme "catppuccin-mocha"
+    end,
+  }
+}
