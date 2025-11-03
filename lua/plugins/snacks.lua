bdddb488b408084ec1dac3db092a041683cc2455
Mauro Locatelli <3275600+crivotz@users.noreply.github.com>
2025-11-03 16:22:33 +0100

    snack github

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -183,6 +183,10 @@ return {
     { "<leader>ss", function() Snacks.picker.lsp_symbols({layout = {preset = "vscode", preview = "main"}}) end, desc = "LSP Symbols" },
     { "<leader>sS", function() Snacks.picker.lsp_workspace_symbols() end, desc = "LSP Workspace Symbols" },
     { "<leader>z", function() Snacks.picker.zoxide() end, desc = "Zoxide" },
+    { "<leader>gi", function() Snacks.picker.gh_issue() end, desc = "GitHub Issues (open)" },
+    { "<leader>gI", function() Snacks.picker.gh_issue({ state = "all" }) end, desc = "GitHub Issues (all)" },
+    { "<leader>gp", function() Snacks.picker.gh_pr() end, desc = "GitHub Pull Requests (open)" },
+    { "<leader>gP", function() Snacks.picker.gh_pr({ state = "all" }) end, desc = "GitHub Pull Requests (all)" },
   },
   init = function()
     vim.api.nvim_create_autocmd("User", {
