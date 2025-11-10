970c46d68b20abd1134e2f3744314eadde4a46f7
Mauro Locatelli <3275600+crivotz@users.noreply.github.com>
2025-11-10 21:42:59 +0100

    Fix copilot

--- lua/plugins/AI-copilot.lua
+++ lua/plugins/AI-copilot.lua
@@ -1,10 +1,62 @@
 return {
   "zbirenbaum/copilot.lua",
-  event = "InsertEnter",
+  dependencies = { "copilotlsp-nvim/copilot-lsp" },
   cmd = "Copilot",
-  opts = {
-    suggestion = {
-      auto_trigger = true,
-    },
-  },
+  event = "InsertEnter",
+  config = function()
+    require("copilot").setup({
+      panel = {
+        keymap = {
+          -- jump_next = "<c-j>",
+          -- jump_prev = "<c-k>",
+          -- accept = "<tab>",
+          refresh = "r",
+          open = "<M-CR>",
+
+        },
+      },
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        keymap = {
+          -- accept = "<c-l>",
+          accept = false,
+          -- next = "<c-j>",
+          -- prev = "<c-k>",
+          dismiss = "<Esc>",
+        },
+      },
+      -- nes = {
+      --   enabled = true,
+      --   keymap = {
+      --     accept_and_goto = "<m-l>",
+      --     accept = false,
+      --     dismiss = "<Esc>",
+      --   },
+      -- },
+      filetypes = {
+        -- yaml = true,
+        -- markdown = true,
+        -- help = false,
+        -- gitcommit = false,
+        -- gitrebase = false,
+        -- cvs = false,
+        -- ["."] = false,
+      },
+      copilot_node_command = "node",
+    })
+
+    local opts = { noremap = true, silent = true }
+    vim.api.nvim_set_keymap("n", "<c-s>", ":lua require('copilot.suggestion').toggle_auto_trigger()<CR>", opts)
+
+    vim.keymap.set("i", "<tab>", function()
+      if require("copilot.suggestion").is_visible() then
+        require("copilot.suggestion").accept()
+        return "<Ignore>"
+      end
+      return "<tab>"
+    end, { expr = true, noremap = true })
+
+    -- vim.g.copilot_nes_debounce = 100
+  end,
 }
