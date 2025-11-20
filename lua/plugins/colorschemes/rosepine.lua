56fd07c03d6747882b628c1b19423f12147fff64
Mauro Locatelli <3275600+crivotz@users.noreply.github.com>
2025-11-20 13:47:05 +0100

    Rose-pine

--- lua/plugins/colorschemes/rosepine.lua
+++ lua/plugins/colorschemes/rosepine.lua
@@ -5,7 +5,7 @@ return {
     priority = 1000,
     config = function()
       require("rose-pine").setup({
-        variant = "moon",
+        -- variant = "moon",
         -- variant = "dawn",
       })
       vim.cmd("colorscheme rose-pine")
