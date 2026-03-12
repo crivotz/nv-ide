279b4cf71b2ff0fc75ffac8b1f46a2470870a70a
Mauro Locatelli <3275600+crivotz@users.noreply.github.com>
2026-03-12 15:30:46 +0100

    Fix dadbod-grip

---
+++ lua/plugins/blink.lua
@@ -0,0 +1,10 @@
+return {
+  "saghen/blink.nvim",
+  opts = {
+    sources = {
+      providers = {
+        dadbod_grip = { name = "Grip SQL", module = "dadbod-grip.completion.blink" },
+      },
+    },
+  },
+}
--- lua/plugins/dadbod-grip.lua
+++ lua/plugins/dadbod-grip.lua
@@ -1,4 +1,15 @@
 return {
   "joryeugene/dadbod-grip.nvim",
-  version = "*",
+  keys = {
+    { "<leader>db", "<cmd>GripToggle<cr>", desc = "DB connect" },
+    { "<leader>dg", "<cmd>Grip<cr>", desc = "DB grid" },
+    { "<leader>dt", "<cmd>GripTables<cr>", desc = "DB tables" },
+    { "<leader>dq", "<cmd>GripQuery<cr>", desc = "DB query pad" },
+    { "<leader>ds", "<cmd>GripSchema<cr>", desc = "DB schema" },
+    { "<leader>dh", "<cmd>GripHistory<cr>", desc = "DB history" },
+  },
+  opts = {
+    connections_path = vim.fn.stdpath("config") .. "/grip/connections.json",
+    keymaps = { qpad_execute = "<C-r>" }, -- or whatever key you prefer
+  },
 }
