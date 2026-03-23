d5bff1941c2c6ac5a660c3e48b738c7e417a189e
Mauro Locatelli <3275600+crivotz@users.noreply.github.com>
2026-03-23 11:37:21 +0100

    wrap and snack

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -40,7 +40,7 @@ return {
       },
       sections = {
         { section = "header" },
-        { section = "keys", gap = 1, padding = 1 },
+        { section = "keys", padding = 1 },
         { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
         { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
         { section = "startup", padding = 1 },
