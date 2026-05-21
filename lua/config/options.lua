51b1e109b4a12d79cee860e4a8b3b6563ae6005d
Mauro Locatelli <3275600+crivotz@users.noreply.github.com>
2026-05-21 15:05:22 +0200

    remove autoformat

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,4 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.wrap = true
+vim.g.autoformat = false
